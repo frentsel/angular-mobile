f824871e652755bef6280299cf22ade8a87f2455
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2018-11-30 22:19:51 +0200

    added lib folder and icom nomponent

---
+++ src/app/lib/icon/icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss']
+})
+export class IconComponent {
+}
